02d9112130f3e9d40913e28d865528305585496d
Karrrrmen <113360429+Karrrrmen@users.noreply.github.com>
2024-11-04 16:49:20 -0500

    Update 03-clean_data.R

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,11 +1,10 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleaned the raw data by selecting needed columns and rows, as well as create duration.
+# Author: Manjun Zhu
+# Date: 3 November 2024
+# Contact: karmen.zhu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: raw data needed
 
 #### Workspace setup ####
 library(tidyverse)
